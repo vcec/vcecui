a3adcdf8f4a16560c4e4eb1288b721e67071b892
Bhalchandra Bhosale <BHALCHANDRA.BHOSALE@pb.com>
2017-12-27 14:29:47 +0530

    Footer section added and some UI changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing/app-routing.module';
 import { OwlModule } from 'ngx-owl-carousel';
+import {Ng2PageScrollModule} from 'ng2-page-scroll';
 
 import {LandingComponent} from './components/landing/landing.component';
 import { HomepageComponent } from './components/homepage/homepage.component'
@@ -21,7 +22,8 @@ import { HomepageComponent } from './components/homepage/homepage.component'
     BrowserModule,
     FormsModule,
     AppRoutingModule,
-    OwlModule
+    OwlModule,
+    Ng2PageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
