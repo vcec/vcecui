1ddf6d80aa517d608582793f2e64da6e34a98353
Bhalchandra Bhosale <BHALCHANDRA.BHOSALE@pb.com>
2018-01-05 15:50:30 +0530

    Video modal added and some UI changes

--- src/app/components/profile/profile.component.ts
+++ src/app/components/profile/profile.component.ts
@@ -1,18 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewEncapsulation } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['./profile.component.scss']
+  styleUrls: ['./profile.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit() {
   }
 
   getAnimationDelay(i,col){
   	return {'animation-delay': ((i+1)%(col+1))*2/10 + 's'};
+  }
+
+  openVideo(content){
+  	this.modalService.open(content, { windowClass: 'video-modal modal-fullscreen'});
   }
 }
