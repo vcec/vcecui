9d729cd032cbfdd71a9dd5df5778a9362ffbcb57
Sandhya Ankush Shitole <Sandhya.Shitole@pb.com>
2018-01-08 14:51:02 +0530

    admin group updates

--- src/app/admin/groups/create-or-update-group/create-or-update-group.component.ts
+++ src/app/admin/groups/create-or-update-group/create-or-update-group.component.ts
@@ -1,15 +1,100 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild, ViewContainerRef, ViewEncapsulation} from '@angular/core';
+import {Router, Route, ActivatedRoute} from "@angular/router";
+import {DataService} from "../../../services/dataService.service";
+import {Config} from "../../../services/config.service";
+import {ToastsManager} from 'ng2-toastr/ng2-toastr';
+import {NgForm} from "@angular/forms";
+
+declare var $: any;
 
 @Component({
   selector: 'app-create-or-update-group',
   templateUrl: './create-or-update-group.component.html',
-  styleUrls: ['./create-or-update-group.component.scss']
+  styles: [`
+    .editGroup .dz-preview {
+      display: none !important;
+    }
+  `],
+  encapsulation: ViewEncapsulation.None
 })
-export class CreateOrUpdateGroupComponent implements OnInit {
 
-  constructor() { }
+export class CreateOrUpdateGroupComponent implements OnInit {
+  @ViewChild('f') groupForm: NgForm;
+  private Id: string;
+  editGroupState = false;
+  addGroupState = false;
+  groupToEdit = {};
+  image: string = '';
+  imageUploadConfig = {
+    url: this.config.serverUrl + 'upload/uploadImage',
+    acceptedFiles: 'image/*'
+  };
+
+  constructor(private router: Router, private route: ActivatedRoute, private dataService: DataService,
+              private config: Config, public toastr: ToastsManager, vcr: ViewContainerRef) {
+    this.route.params.subscribe(params => {
+      this.Id = params['id'];
+    });
+    this.toastr.setRootViewContainerRef(vcr);
+  }
 
   ngOnInit() {
+    if (this.Id) {
+      this.editGroupState = true;
+      this.dataService.getGroupById(this.Id).subscribe((res) => {
+        this.groupToEdit = res['data'];
+        this.image = this.groupToEdit['img'];
+      }, (err) => {
+
+      });
+    } else {
+      this.addGroupState = true;
+    }
+  }
+
+
+  onImageUploadError(event) {
+    console.log(event);
+  }
+
+  onRemoved() {
+    this.image = "";
+  }
+
+  onImageUploadSuccess(event) {
+    if (event[1].data.urlPath) {
+      this.image = event[1].data.urlPath;
+    }
+  }
+
+  onCreate() {
+    let data = $.extend(this.groupForm.value, {img: this.image});
+    this.dataService.saveGroup(data).subscribe((res) => {
+      this.router.navigate(['../'], {relativeTo: this.route})
+    }, (err) => {
+      if (err.status === 0) {
+        this.toastr.error("Server is Down.")
+      } else {
+        this.toastr.error(err.message);
+      }
+    });
+  }
+
+  onUpdate() {
+    let data = {
+      group_name: this.groupForm.controls['groupToEdit.group_name'].value,
+      img: this.image
+    };
+
+    this.dataService.updateGroup(this.groupToEdit['_id'], data).subscribe((res) => {
+      this.router.navigate(['../../'], {relativeTo: this.route});
+    }, (err) => {
+      if (err.status === 0) {
+        this.toastr.error("Server is Down.")
+      } else {
+        this.toastr.error(err.message);
+      }
+    });
   }
 
 }
--- src/app/admin/groups/groups.component.ts
+++ src/app/admin/groups/groups.component.ts
@@ -1,60 +1,29 @@
 import {Component, OnInit, ViewChild, ViewContainerRef, ViewEncapsulation} from '@angular/core';
 import {DataService} from '../../services/dataService.service';
-import {NgForm} from "@angular/forms";
 import {Config} from "../../services/config.service";
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {ToastsManager} from 'ng2-toastr/ng2-toastr';
 import {DialogService} from "ng2-bootstrap-modal";
+import {Router, Route, ActivatedRoute} from "@angular/router";
 import {ConfirmComponent} from "../confirmComponent/confirm.component";
-
 
 declare var $: any;
 
 @Component({
   selector: 'app-groups',
-  templateUrl: './groups.component.html',
-  styles: [`
-    .editGroup .dz-preview {
-      display: none !important;
-    }
-  `],
-  encapsulation: ViewEncapsulation.None
+  templateUrl: './groups.component.html'
 })
 
 export class GroupsComponent implements OnInit {
-  @ViewChild('f') groupForm: NgForm;
-  addGroupState = false;
-  editGroupState = false;
-  groupToEdit = {};
-  newGroup = {};
   groups: any[] = [];
-  image: string = '';
-  imageUploadConfig = {
-    url: this.config.serverUrl + 'upload/uploadImage',
-    acceptedFiles: 'image/*'
-  };
 
-  constructor(private dataService: DataService, private config: Config,
+  constructor(private dataService: DataService, private config: Config, private router: Router, private route: ActivatedRoute,
               public toastr: ToastsManager, vcr: ViewContainerRef, private dialogService: DialogService) {
     this.toastr.setRootViewContainerRef(vcr);
   }
 
   ngOnInit() {
     this.getAllGroups();
-  }
-
-  onImageUploadError(event) {
-    console.log(event);
-  }
-
-  onRemoved() {
-    this.image = "";
-  }
-
-  onImageUploadSuccess(event) {
-    if (event[1].data.urlPath) {
-      this.image = event[1].data.urlPath;
-    }
   }
 
   getAllGroups() {
@@ -72,53 +41,14 @@ export class GroupsComponent implements OnInit {
     );
   }
 
-  onUpdate() {
-    let data = {
-      group_name: this.groupForm.controls['groupToEdit.group_name'].value,
-      img: this.image
-    };
 
-    this.dataService.updateGroup(this.groupToEdit['_id'], data).subscribe((res) => {
-      this.toastr.success("Group updated successfully.", null, {toastLife: 3000});
-      this.image = "";
-      this.groupToEdit = {};
-      this.editGroupState = false;
-      this.getAllGroups();
-    }, (err) => {
-      if (err.status === 0) {
-        this.toastr.error("Server is Down.")
-      } else {
-        this.toastr.error(err.message);
-      }
-    });
+  addGroup() {
+    this.router.navigate(['./create'], {relativeTo: this.route});
   }
+
 
   editGroup(group) {
-    this.addGroupState = false;
-    this.resetForm();
-    this.groupToEdit = this.groups.filter(function (v, i) {
-      return v.productGroupId === group.productGroupId ? true : false;
-    });
-    this.groupToEdit = $.extend(this.groupToEdit[0]);
-    this.image = this.groupToEdit['img'];
-    this.editGroupState = true;
-  }
-
-  onCreate() {
-    let data = $.extend(this.groupForm.value, {img: this.image});
-    this.dataService.saveGroup(data).subscribe((res) => {
-      this.toastr.success("Group created successfully.", null, {toastLife: 3000});
-      this.image = "";
-      this.newGroup = {};
-      this.addGroupState = false;
-      this.getAllGroups();
-    }, (err) => {
-      if (err.status === 0) {
-        this.toastr.error("Server is Down.")
-      } else {
-        this.toastr.error(err.message);
-      }
-    });
+    this.router.navigate(['./update', group._id], {relativeTo: this.route});
   }
 
   deleteGroup(group) {
@@ -139,9 +69,5 @@ export class GroupsComponent implements OnInit {
         });
       }
     });
-  }
-
-  resetForm() {
-    this.image = "";
   }
 }
